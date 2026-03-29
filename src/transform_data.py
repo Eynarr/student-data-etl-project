edf560a1706778c695fe3773bd71d27082e64a5d
Eynarr <moraleseynar13@gmail.com>
2026-03-28 20:23:49 -0500

    feat: improve data cleaning script

--- src/transform_data.py
+++ src/transform_data.py
@@ -1,37 +1,74 @@
 import pandas as pd
 
-# Read the students csv
-df = pd.read_csv("../data/raw/students.csv")
 
-# Clean columns names
-df.columns = df.columns.str.strip().str.lower()
+def transform_data(input_path, output_path):
+    # Read the students csv
+    df = pd.read_csv(input_path)
 
-# Select only relevant columns
-columns_to_keep = [
-    "inter_dom",
-    "region",
-    "gender",
-    "academic",
-    "age",
-    "stay",
-    "japanese_cate",
-    "english_cate",
-    "todep",
-    "tosc",
-    "toas",
-]
-df = df[columns_to_keep]
+    # Drop missing values and duplicates
+    df = df.dropna(how="all")
+    df = df.drop_duplicates()
 
-# Drop missing values
-df = df.dropna()
+    # Clean columns names
+    df.columns = df.columns.str.strip().str.lower()
 
-# Normalized academic categories
-df["academic"] = df["academic"].replace({"Grad": "Graduated", "Under": "Undergraduate"})
+    # Select only relevant columns
+    columns_to_keep = [
+        "inter_dom",
+        "region",
+        "gender",
+        "academic",
+        "age",
+        "stay",
+        "japanese_cate",
+        "english_cate",
+        "todep",
+        "tosc",
+        "toas",
+    ]
+    df = df[columns_to_keep]
 
-# Boolean column to identified international students
-df["is_international"] = df["inter_dom"] == "Inter"
+    # Rename columns for clarity
+    df = df.rename(
+        columns={
+            "stay": "years_country",
+            "inter_dom": "student_type",
+            "todep": "depression_score",
+            "tosc": "social_connectedness_score",
+            "toas": "acculturative_stress_score",
+            "academic": "academic_level",
+            "english_cate": "english_level",
+            "japanese_cate": "japanese_level",
+        }
+    )
 
-# Saving the new csv
-df.to_csv("../data/processed/student_clean.csv", index=False)
+    # Normalized data
+    text_cols = ["student_type", "academic_level", "english_level", "japanese_level"]
 
-print("Cleaned data saved")
+    for col in text_cols:
+        df[col] = df[col].str.strip().str.title()
+
+    df["academic_level"] = df["academic_level"].replace(
+        {"Grad": "Graduated", "Under": "Undergraduate"}
+    )
+
+    df["student_type"] = df["student_type"].replace(
+        {"Inter": "International", "Dom": "Domestic"}
+    )
+
+    # Type conversion for clarity
+    df["years_country"] = pd.to_numeric(df["years_country"], errors="coerce")
+    df["age"] = pd.to_numeric(df["age"], errors="coerce")
+
+    df = df.dropna(subset=["years_country", "age"])
+
+    # Saving the new csv
+    df.to_csv(output_path, index=False)
+    print("Cleaned dataset saved")
+    return df
+
+
+df = transform_data(
+    "../data/raw/students.csv",
+    "../data/processed/students.csv",
+)
