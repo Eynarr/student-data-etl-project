06cf8caa28377c9be725756f9df59edb4be6d838
Eynarr <moraleseynar13@gmail.com>
2026-04-25 22:17:08 -0500

    feat: add comparison between groups query

--- sql/analysis.sql
+++ sql/analysis.sql
@@ -31,3 +31,18 @@ FROM students
 SELECT *
 FROM students s, stats_percentile sp
 WHERE s.depression_score >= sp.p75 AND s.social_connectedness_score <= 21;
+
+-- 4. Comparison between student groups
+
+WITH avg_type_depression AS (
+SELECT ROUND((AVG(depression_score) FILTER (WHERE student_type = 'Domestic'))::NUMERIC, 2) AS avg_dom_depression,
+       ROUND((AVG(depression_score) FILTER (WHERE student_type = 'International'))::NUMERIC, 2) AS avg_inter_depression
+FROM students)
+SELECT *,
+	   ROUND(ABS(avg_dom_depression - avg_inter_depression)::NUMERIC,2) AS diff_depression_groups,
+	   CASE
+	   		WHEN avg_dom_depression > avg_inter_depression THEN 'Domestic higher'
+	   		WHEN avg_dom_depression < avg_inter_depression THEN 'Internatinal higher'
+	   		ELSE 'Equal'
+	   END
+FROM avg_type_depression;
